1d76b04219186c788e82d1f910f38d73501142ee
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-10-06 20:40:37 +0300

    First attempt with detection
    This is also work in progress

--- src/PVRGlobal.cs
+++ src/PVRGlobal.cs
@@ -1,11 +1,54 @@
+using System;
+using System.IO;
 
 namespace CSharp_PVR
 {
+	public enum PvrVersion
+	{
+		Unknown = 0,
+		Version1 = 1,
+		Version2 = 2,
+		Version3 = 3,
+	}
+
 	public static class PVRGlobal
 	{
-		public static bool IsV3Format()
+		public static PvrVersion DetectVersion(Stream inputStream)
 		{
-			return true;
+			if (IsV3FormatFile(inputStream, returnToOriginalPosition: true))
+			{
+				return PvrVersion.Version3;
+			}
+
+			return PvrVersion.Unknown;
+		}
+
+		public static bool IsV3FormatFile(Stream inputStream, bool returnToOriginalPosition = true)
+		{
+			if (returnToOriginalPosition && !inputStream.CanSeek)
+			{
+				throw new ArgumentException("Cannot return to original position since inputStream does not support seeking!");
+			}
+
+			long originalPosition = inputStream.Position;
+
+			bool returnValue = true;
+
+			try
+			{
+				PVRContainerV3 containerV3 = new PVRContainerV3(inputStream);
+			}
+			catch
+			{
+				returnValue = false;
+			}
+
+			if (returnToOriginalPosition)
+			{
+				inputStream.Position = originalPosition;
+			}
+
+			return returnValue;
 		}
 	}
 }
