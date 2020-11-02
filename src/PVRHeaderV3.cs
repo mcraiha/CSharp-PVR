cce501451bfd3265d9b7bf21a71c8d2dc593d7fd
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-11-02 20:10:07 +0200

    More enum documentation
    For V3

--- src/PVRHeaderV3.cs
+++ src/PVRHeaderV3.cs
@@ -3,6 +3,9 @@ using System.IO;
 
 namespace CSharp_PVR
 {
+	/// <summary>
+	/// Texture flags
+	/// </summary>
 	public enum TextureFlags : uint
 	{
 		/// <summary>
@@ -16,92 +19,387 @@ namespace CSharp_PVR
 		PreMultiplied = 0x02
 	}
 
+	/// <summary>
+	/// Pixel format V3
+	/// </summary>
 	public enum PixelFormatV3 : ulong
 	{
+		/// <summary>
+		/// PVRTC 2bpp RGB
+		/// </summary>
 		PVRTC2bppRGB = 0,
+
+		/// <summary>
+		/// PVRTC 2bpp RGBA
+		/// </summary>
 		PVRTC2bppRGBA = 1,
+
+		/// <summary>
+		/// PVRTC 4bpp RGB
+		/// </summary>
 		PVRTC4bppRGB = 2,
+
+		/// <summary>
+		/// PVRTC 4bpp RGBA
+		/// </summary>
 		PVRTC4bppRGBA = 3,
+
+		/// <summary>
+		/// PVRTC-II 2bpp
+		/// </summary>
 		PVRTCII2bpp = 4,
+
+		/// <summary>
+		/// PVRTC-II 4bpp
+		/// </summary>
 		PVRTCII4bpp = 5,
+
+		/// <summary>
+		/// ETC1
+		/// </summary>
 		ETC1 = 6,
+
+		/// <summary>
+		/// DXT1
+		/// </summary>
 		DXT1 = 7,
+
+		/// <summary>
+		/// DXT2
+		/// </summary>
 		DXT2 = 8,
+
+		/// <summary>
+		/// DXT3
+		/// </summary>
 		DXT3 = 9,
+
+		/// <summary>
+		/// DXT4
+		/// </summary>
 		DXT4 = 10,
+
+		/// <summary>
+		/// DXT5
+		/// </summary>
 		DXT5 = 11,
+
+		/// <summary>
+		/// BC1
+		/// </summary>
 		BC1 = 7,
+
+		/// <summary>
+		/// BC2
+		/// </summary>
 		BC2 = 9,
+
+		/// <summary>
+		/// BC3
+		/// </summary>
 		BC3 = 11,
+
+		/// <summary>
+		/// BC4
+		/// </summary>
 		BC4 = 12,
+
+		/// <summary>
+		/// BC5
+		/// </summary>
 		BC5 = 13,
+
+		/// <summary>
+		/// BC6
+		/// </summary>
 		BC6 = 14,
+
+		/// <summary>
+		/// BC7
+		/// </summary>
 		BC7 = 15,
+
+		/// <summary>
+		/// UYVY
+		/// </summary>
 		UYVY = 16,
+
+		/// <summary>
+		/// YUY2
+		/// </summary>
 		YUY2 = 17,
+
+		/// <summary>
+		/// BW1bpp
+		/// </summary>
 		BW1bpp = 18,
+
+		/// <summary>
+		/// R9G9B9E5 Shared Exponent
+		/// </summary>
 		R9G9B9E5SharedExponent = 19,
+
+		/// <summary>
+		/// RGBG8888
+		/// </summary>
 		RGBG8888 = 20,
+
+		/// <summary>
+		/// GRGB8888
+		/// </summary>
 		GRGB8888 = 21,
+
+		/// <summary>
+		/// ETC2 RGB
+		/// </summary>
 		ETC2RGB = 22,
+
+		/// <summary>
+		/// ETC2 RGBA
+		/// </summary>
 		ETC2RGBA = 23,
+
+		/// <summary>
+		/// ETC2 RGB A1
+		/// </summary>
 		ETC2RGBA1 = 24,
+
+		/// <summary>
+		/// EAC R11
+		/// </summary>
 		EACR11 = 25,
+
+		/// <summary>
+		/// EAC RG11
+		/// </summary>
 		EACRG11 = 26,
+
+		/// <summary>
+		/// ASTC_4x4
+		/// </summary>
 		ASTC4x4 = 27,
+
+		/// <summary>
+		/// ASTC_5x4
+		/// </summary>
 		ASTC5x4 = 28,
+
+		/// <summary>
+		/// ASTC_5x5
+		/// </summary>
 		ASTC5x5 = 29,
+
+		/// <summary>
+		/// ASTC_6x5
+		/// </summary>
 		ASTC6x5 = 30,
+
+		/// <summary>
+		/// ASTC_6x6
+		/// </summary>
 		ASTC6x6 = 31,
+
+		/// <summary>
+		/// ASTC_8x5
+		/// </summary>
 		ASTC8x5 = 32,
+
+		/// <summary>
+		/// ASTC_8x6
+		/// </summary>
 		ASTC8x6 = 33,
+
+		/// <summary>
+		/// ASTC_8x8
+		/// </summary>
 		ASTC8x8 = 34,
+
+		/// <summary>
+		/// ASTC_10x5
+		/// </summary>
 		ASTC10x5 = 35,
+
+		/// <summary>
+		/// ASTC_10x6
+		/// </summary>
 		ASTC10x6 = 36,
+
+		/// <summary>
+		/// ASTC_10x8
+		/// </summary>
 		ASTC10x8 = 37,
+
+		/// <summary>
+		/// ASTC_10x10
+		/// </summary>
 		ASTC10x10 = 38,
+
+		/// <summary>
+		/// ASTC_12x10
+		/// </summary>
 		ASTC12x10 = 39,
+
+		/// <summary>
+		/// ASTC_12x12
+		/// </summary>
 		ASTC12x12 = 40,
+
+		/// <summary>
+		/// ASTC_3x3x3
+		/// </summary>
 		ASTC3x3x3 = 41,
+
+		/// <summary>
+		/// ASTC_4x3x3
+		/// </summary>
 		ASTC4x3x3 = 42,
+
+		/// <summary>
+		/// ASTC_4x4x3
+		/// </summary>
 		ASTC4x4x3 = 43,
+
+		/// <summary>
+		/// ASTC_4x4x4
+		/// </summary>
 		ASTC4x4x4 = 44,
+
+		/// <summary>
+		/// ASTC_5x4x4
+		/// </summary>
 		ASTC5x4x4 = 45,
+
+		/// <summary>
+		/// ASTC_5x5x4
+		/// </summary>
 		ASTC5x5x4 = 46,
+
+		/// <summary>
+		/// ASTC_5x5x5
+		/// </summary>
 		ASTC5x5x5 = 47,
+
+		/// <summary>
+		/// ASTC_6x5x5
+		/// </summary>
 		ASTC6x5x5 = 48,
+
+		/// <summary>
+		/// ASTC_6x6x5
+		/// </summary>
 		ASTC6x6x5 = 49,
+
+		/// <summary>
+		/// ASTC_6x6x6
+		/// </summary>
 		ASTC6x6x6 = 50,
 	}
 
+	/// <summary>
+	/// Color space
+	/// </summary>
 	public enum ColorSpace : uint
 	{
+		/// <summary>
+		/// Texture data is in the Linear RGB colour space
+		/// </summary>
 		LinearRGB = 0,
+
+		/// <summary>
+		/// Texture data is in the Standard RGB colour space
+		/// </summary>
 		sRGB = 1
 	}
 
+	/// <summary>
+	/// Channel type
+	/// </summary>
 	public enum ChannelType : uint
 	{
+		/// <summary>
+		/// Unsigned Byte Normalised 
+		/// </summary>
 		UnsignedByteNormalised = 0,
+
+		/// <summary>
+		/// Signed Byte Normalised
+		/// </summary>
 		SignedByteNormalised = 1,
+
+		/// <summary>
+		/// Unsigned Byte
+		/// </summary>
 		UnsignedByte = 2,
+
+		/// <summary>
+		/// Signed Byte
+		/// </summary>
 		SignedByte = 3,
+
+		/// <summary>
+		/// Unsigned Short Normalised
+		/// </summary>
 		UnsignedShortNormalised = 4,
+
+		/// <summary>
+		/// Signed Short Normalised
+		/// </summary>
 		SignedShortNormalised = 5,
+
+		/// <summary>
+		/// Unsigned Short
+		/// </summary>
 		UnsignedShort = 6,
+
+		/// <summary>
+		/// Signed Short
+		/// </summary>
 		SignedShort = 7,
+
+		/// <summary>
+		/// Unsigned Integer Normalised
+		/// </summary>
 		UnsignedIntegerNormalised = 8,
+
+		/// <summary>
+		/// Signed Integer Normalised
+		/// </summary>
 		SignedIntegerNormalised = 9,
+
+		/// <summary>
+		/// Unsigned Integer
+		/// </summary>
 		UnsignedInteger = 10,
+
+		/// <summary>
+		/// Signed Integer
+		/// </summary>
 		SignedInteger = 11,
+
+		/// <summary>
+		/// Float
+		/// </summary>
 		Float = 12,
 	}
 
+	/// <summary>
+	/// PVR Header V3
+	/// </summary>
 	sealed public class PVRHeaderV3
 	{
+		/// <summary>
+		/// Only valid header size in bytes
+		/// </summary>
 		public const int headerSizeInBytes = 52;
 
+		/// <summary>
+		/// Matching endianness
+		/// </summary>
 		public const uint matchingEndianness = 0x03525650;
+
+		/// <summary>
+		/// Endianness needing reverse
+		/// </summary>
 		public const uint notMatchingEndianness = 0x50565203;
 
 		private readonly TextureFlags textureFlags;
